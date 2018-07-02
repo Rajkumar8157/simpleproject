68bad9bdf98dbfd1bb6d602a781e1625a3aab796
Rajkumar M <rajkumar8157@gmail.com>
2018-07-02 23:34:05 +0530

    added verify title

--- src/main/java/simpleproject/NewTest.java
+++ src/main/java/simpleproject/NewTest.java
@@ -1,5 +1,6 @@
 package simpleproject;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
@@ -11,8 +12,13 @@ public class NewTest {
 	public WebDriver driver;
 	 
 	  @Test
-	  public void openMyBlog() {
+	  public void openMyBlog() throws InterruptedException {
 	 driver.get("https://www.softwaretestingmaterial.com/");
+	 Thread.sleep(100);
+	 driver.findElement(By.xpath("//img[@alt='Selenium Tutorial']")).click();
+	 Thread.sleep(100);
+	 String title = driver.getTitle();
+	 System.out.println("Title is ="+title);
 	  }
 	  
 	  @BeforeClass
